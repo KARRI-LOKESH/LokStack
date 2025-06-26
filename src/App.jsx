0848715997485f743a4dffeb7c4a0cc8cbde5608
karri-lokesh <karrilokesh108@gmail.com>
2025-06-26 23:45:20 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function AppWrapper() {
             <Link to="/about" className="nav-link" onClick={handleNavLinkClick}>About</Link>
             <Link to="/contact" className="nav-link" onClick={handleNavLinkClick}>Contact</Link>
             {loggedIn && (
-              <button className="nav-link" onClick={handleLogout}>Logout</button>
+              <Link to="/login" className="nav-link" onClick={handleLogout}>Logout</Link>
             )}
           </nav>
         </header>
